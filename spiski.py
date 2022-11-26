5af390d8c3880b2cde0d549317a972df31561c4c
Nurkaliev <nurkaliev2005.19@gmail.com>
2022-11-26 14:02:03 +0600

    spisok

---
+++ spiski.py
@@ -0,0 +1,23 @@
+'''
+list = [3, True, 'Витя', 2.0,10000,{6:7}] 
+'''
+'''
+p = 0
+names = ['JACK', 'JIMMY', 'JACKSON', 'JHON', 'JACKSON', 'JHON',  'JIMMY', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON', 'JACKSON', 'JHON','JACK', 'JIMMY', 'JACK', 'JACKSON', 'JHON',]
+for i in names:
+	if i=='JACK':
+		p+=1
+print(p)
+'''
+'''
+a=[]
+a.append('iskhak')
+a.append('2005')
+a.append('python')
+print(a)
+'''
+'''
+names = ['aza', 'nurs', 'take', 'shama', 'isa']
+list=''.join(names)
+print(list)
+'''
